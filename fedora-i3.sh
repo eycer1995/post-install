56eba7776d8ac0d72da01751782a10ddd6f5b66e
Eycer L <eycer1995@gmail.com>
2023-03-01 10:47:17 -0300

    wip

--- fedora-i3.sh
+++ fedora-i3.sh
@@ -6,21 +6,23 @@ sudo dnf install btop alacritty polybar vim feh ranger git -y
 # Optional
 sudo dnf install cmatrix cava neofetch zsh -y
 
-# Install zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s $(which zsh)
-
+# Get configs files
 git clone https://github.com/eycer1995/configs ~/Documents/configs
-# chown -R $USER:$USER ~/Documents/configs
+
+# Copy dotfiles to /home
+cp -r -p ~/Documents/configs/.configs $HOME
+cp -p ~/Documents/configs/.vimrc $HOME
+cp -p ~/Documents/configs/.bashrc $HOME
+
+# Clean configs
+rm -r ~/Documents/configs
 
 # Download wallpaper
 wget https://w.wallhaven.cc/full/r2/wallhaven-r27x11.jpg -O ~/Pictures/blue.jpg
 
-# Copy dotfiles to /home
-cp -r -p ~/Documents/configs/. $HOME
-rm -r ~/Documents/configs
+# Install ohmyzsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
 
 # Reload i3
-# runuser -l eycer -c 'vim +PluginInstall +qall'
-vim +PluginInstall +qall
 echo "Done. Please reload i3wm"
